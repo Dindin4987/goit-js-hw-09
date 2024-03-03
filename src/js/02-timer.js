758a5e0348cb982c7a68660bef66ad994e639e75
Dindin4987 <144334821+Dindin4987@users.noreply.github.com>
2024-03-03 14:04:49 +0800

    update task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,26 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+
+const refs = {
+    datetimePicker: document.querySelector('input#datetime-picker'),
+    startButton: document.querySelector('button[data-start]'),
+    days: document.querySelector('span[data-days]')
+    hours: document.querySelector('span[data-hours]')
+    minutes: document.querySelector('span[data-minutes]')
+    seconds: document.querySelector('span[data-seconds]')
+};
+
+refs.startButton.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr("#datetime-pciker", options);
